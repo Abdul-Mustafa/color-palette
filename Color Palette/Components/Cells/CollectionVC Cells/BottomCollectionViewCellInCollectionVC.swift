1d95db35131a5ed105f54beaa2961b85c26f1409
Abdul Mustafa <mac@zulfi-mac.local>
2025-03-19 11:16:18 +0500

    Now button are showing and appearing just on selection my next move is to change their color according to view color and also functions are not working yet.

--- Color Palette/Components/Cells/CollectionVC Cells/BottomCollectionViewCellInCollectionVC.swift
+++ Color Palette/Components/Cells/CollectionVC Cells/BottomCollectionViewCellInCollectionVC.swift
@@ -1,12 +1,3 @@
-import UIKit
-
-class BottomCollectionViewCellInCollectionVC: UICollectionViewCell {
-    @IBOutlet weak var colorViewInBottomCollectionViewCell: UIView!
-    @IBOutlet weak var colorNameInHexaCodeFormatInBottomCollectionViewCell: UILabel!
-    @IBOutlet weak var viewWithColorLabel: UIView!
-    
-}
-
 //import UIKit
 //
 //class BottomCollectionViewCellInCollectionVC: UICollectionViewCell {
@@ -14,81 +5,100 @@ class BottomCollectionViewCellInCollectionVC: UICollectionViewCell {
 //    @IBOutlet weak var colorNameInHexaCodeFormatInBottomCollectionViewCell: UILabel!
 //    @IBOutlet weak var viewWithColorLabel: UIView!
 //    
-//    private let heartButton: UIButton = {
-//        let button = UIButton(type: .system)
-//        button.translatesAutoresizingMaskIntoConstraints = false
-//        return button
-//    }()
-//    
-//    private let copyButton: UIButton = {
-//        let button = UIButton(type: .system)
-//        button.translatesAutoresizingMaskIntoConstraints = false
-//        return button
-//    }()
-//    
-//    private let shareButton: UIButton = {
-//        let button = UIButton(type: .system)
-//        button.translatesAutoresizingMaskIntoConstraints = false
-//        return button
-//    }()
-//    
-//    private var centerYConstraint: NSLayoutConstraint?
-//    
-//    override func awakeFromNib() {
-//        super.awakeFromNib()
-//        setupButtons()
-//        updateButtonVisibility()
-//    }
-//    
-//    private func setupButtons() {
-//        colorViewInBottomCollectionViewCell.addSubview(heartButton)
-//        colorViewInBottomCollectionViewCell.addSubview(copyButton)
-//        colorViewInBottomCollectionViewCell.addSubview(shareButton)
-//        
-//        let stackView = UIStackView(arrangedSubviews: [heartButton, copyButton, shareButton])
-//        stackView.axis = .vertical
-//        stackView.alignment = .center
-//        stackView.distribution = .equalSpacing
-//        stackView.spacing = 5
-//        stackView.translatesAutoresizingMaskIntoConstraints = false
-//        
-//        colorViewInBottomCollectionViewCell.addSubview(stackView)
-//        
-//        // Store the centerY constraint to update later
-//        centerYConstraint = stackView.centerYAnchor.constraint(equalTo: colorViewInBottomCollectionViewCell.centerYAnchor)
-//        
-//        NSLayoutConstraint.activate([
-//            stackView.centerXAnchor.constraint(equalTo: colorViewInBottomCollectionViewCell.centerXAnchor),
-//            centerYConstraint!,
-//            
-//            heartButton.widthAnchor.constraint(equalTo: colorViewInBottomCollectionViewCell.widthAnchor, multiplier: 0.17),
-//            heartButton.heightAnchor.constraint(equalTo: heartButton.widthAnchor),
-//            
-//            copyButton.widthAnchor.constraint(equalTo: colorViewInBottomCollectionViewCell.widthAnchor, multiplier: 0.17),
-//            copyButton.heightAnchor.constraint(equalTo: copyButton.widthAnchor),
-//            
-//            shareButton.widthAnchor.constraint(equalTo: colorViewInBottomCollectionViewCell.widthAnchor, multiplier: 0.17),
-//            shareButton.heightAnchor.constraint(equalTo: shareButton.widthAnchor)
-//        ])
-//    }
-//    
-//    override func layoutSubviews() {
-//        super.layoutSubviews()
-//        // Update the centerY constraint with the offset once the layout is ready
-//        if let constraint = centerYConstraint {
-//            constraint.constant = -viewWithColorLabel.frame.height / 2
-//        }
-//    }
-//    
-//    private func updateButtonVisibility() {
-//        heartButton.isHidden = !isSelected
-//        copyButton.isHidden = !isSelected
-//        shareButton.isHidden = !isSelected
-//    }
-//    
-//    override var isSelected: Bool {
-//        didSet {
-//            updateButtonVisibility()
-//        }
-//    }
 //}
+
+
+import UIKit
+
+class BottomCollectionViewCellInCollectionVC: UICollectionViewCell {
+    @IBOutlet weak var colorViewInBottomCollectionViewCell: UIView!
+    @IBOutlet weak var colorNameInHexaCodeFormatInBottomCollectionViewCell: UILabel!
+    @IBOutlet weak var viewWithColorLabel: UIView!
+    
+    private let heartButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let copyButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let shareButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private var centerYConstraint: NSLayoutConstraint?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupButtons()
+        updateButtonVisibility()
+        
+        // Pin colorViewInBottomCollectionViewCell to contentView
+        colorViewInBottomCollectionViewCell.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            colorViewInBottomCollectionViewCell.topAnchor.constraint(equalTo: contentView.topAnchor),
+            colorViewInBottomCollectionViewCell.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            colorViewInBottomCollectionViewCell.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            colorViewInBottomCollectionViewCell.bottomAnchor.constraint(equalTo: viewWithColorLabel.topAnchor)
+        ])
+    }
+    
+    private func setupButtons() {
+        // Add placeholder images for visibility
+        heartButton.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+        copyButton.setImage(UIImage(systemName: "doc.on.doc"), for: .normal)
+        shareButton.setImage(UIImage(systemName: "square.and.arrow.up"), for: .normal)
+        
+        colorViewInBottomCollectionViewCell.addSubview(heartButton)
+        colorViewInBottomCollectionViewCell.addSubview(copyButton)
+        colorViewInBottomCollectionViewCell.addSubview(shareButton)
+        
+        let stackView = UIStackView(arrangedSubviews: [heartButton, copyButton, shareButton])
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 5
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        colorViewInBottomCollectionViewCell.addSubview(stackView)
+        
+        centerYConstraint = stackView.centerYAnchor.constraint(equalTo: colorViewInBottomCollectionViewCell.centerYAnchor)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: colorViewInBottomCollectionViewCell.centerXAnchor),
+            centerYConstraint!,
+            heartButton.widthAnchor.constraint(equalTo: colorViewInBottomCollectionViewCell.widthAnchor, multiplier: 0.17),
+            heartButton.heightAnchor.constraint(equalTo: heartButton.widthAnchor),
+            copyButton.widthAnchor.constraint(equalTo: colorViewInBottomCollectionViewCell.widthAnchor, multiplier: 0.17),
+            copyButton.heightAnchor.constraint(equalTo: copyButton.widthAnchor),
+            shareButton.widthAnchor.constraint(equalTo: colorViewInBottomCollectionViewCell.widthAnchor, multiplier: 0.17),
+            shareButton.heightAnchor.constraint(equalTo: shareButton.widthAnchor)
+        ])
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        if let constraint = centerYConstraint {
+            constraint.constant = -viewWithColorLabel.frame.height / 2
+        }
+    }
+    
+    private func updateButtonVisibility() {
+         heartButton.isHidden = !isSelected
+         copyButton.isHidden = !isSelected
+         shareButton.isHidden = !isSelected
+    }
+    
+    override var isSelected: Bool {
+        didSet {
+             updateButtonVisibility() // Disabled for testing
+        }
+    }
+}
