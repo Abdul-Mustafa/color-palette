e66b406a95ab1e4d4ccb5918610319bbfdf4911e
Abdul Mustafa <mac@zulfi-mac.local>
2025-03-19 10:35:28 +0500

    In storyboard I shift the Bottom Collection view cell in collection vc from the hom vc for debugging.

--- Color Palette/Components/Cells/CollectionVC Cells/BottomCollectionViewCellInCollectionVC.swift
+++ Color Palette/Components/Cells/CollectionVC Cells/BottomCollectionViewCellInCollectionVC.swift
@@ -1,15 +1,8 @@
-//
-//  BottomCollectionViewCellInCollectionVC.swift
-//  Color Palette
-//
-//  Created by mac on 04/03/2025.
-//
-
 import UIKit
 
 class BottomCollectionViewCellInCollectionVC: UICollectionViewCell {
     @IBOutlet weak var colorViewInBottomCollectionViewCell: UIView!
-    
-    @IBOutlet weak var viewWithColorLabel: UIView!
     @IBOutlet weak var colorNameInHexaCodeFormatInBottomCollectionViewCell: UILabel!
+    @IBOutlet weak var viewWithColorLabel: UIView!
+    
 }
